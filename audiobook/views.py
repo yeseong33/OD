824032d668dddfd4a5b206210e5805567abdf9ba
WooyoungPark <wooyoung9654@gmail.com>
2023-12-19 14:37:42 +0900

    feat: make audiobook urls

--- audiobook/views.py
+++ audiobook/views.py
@@ -7,3 +7,39 @@ def index(request):
 
 def template(request):
     return render(request, 'audiobook/template.html')
+
+
+def main(request):
+    pass
+
+
+def genre(request):
+    pass
+
+
+def search(request):
+    pass
+
+
+def content(request):
+    pass
+
+
+def content_play(request):
+    pass
+
+
+def voice_custom(request):
+    pass
+
+
+def voice_celebrity(request):
+    pass
+
+
+def voice_custom_upload(request):
+    pass
+
+
+def voice_custom_complete(request):
+    pass
