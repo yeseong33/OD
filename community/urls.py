c362110fcd0aeb3d67769c244a461c7f3d5b367a
WooyoungPark <wooyoung9654@gmail.com>
2023-12-20 01:05:12 +0900

    feat: search book using naver api

--- community/urls.py
+++ community/urls.py
@@ -5,5 +5,6 @@ app_name = 'community'
 
 urlpatterns = [
     # path('', views.index, name='community_index'),
-    path('books/search/', views.BookSearchView.as_view(), name='book_search'),
+    path('books/search/', views.BookSearchView.as_view(), name='book_search'),  # http://127.0.0.1:8000/community/books/search
+    path('books/search/<int:isbn>/', views.BookCompleteView.as_view(), name='book_complete'), 
 ]
--- community/views.py
+++ community/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
 import requests
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework import status
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from audiobook.models import Book
+from user.models import User
+from .models import BookRequest, UserRequestBook
+from .serializers import *
+from django.db import transaction
+from django.db.models import F
+
 load_dotenv()  # 환경 변수를 로드함
 
 class BookSearchView(APIView):
@@ -31,3 +39,37 @@ class BookSearchView(APIView):
                 context['error'] = "An error occurred while searching for books."
 
         return Response(context)
+    
+    
+class BookCompleteView(APIView):
+    def get(self, request, isbn):
+        
+        # 로그인 구현 전 커스텀 User 모델을 사용하여 임시 유저 생성
+        # ttemp_user = User.objects.create(
+        #     oauth_provider='some_provider',
+        #     oauth_identifier='some_identifier',  # 필요에 따라 설정
+        #     user_name='new_user_name',
+        #     user_email='user@example.com',
+        #     user_phone_number='000-0000-0000',
+        #     user_book_history = [1, 2, 3], 
+        #     user_favorite_books = [4, 5, 6],
+        #     user_favorite_voices = [7, 8, 9],
+        # )
+        # ttemp_user.save()
+        ttemp_user = User.objects.get(user_id=1)
+        
+        book_request, created = BookRequest.objects.get_or_create(request_isbn=isbn, defaults={'request_count': 0})
+        
+        # Atomically increment the request_count to ensure accuracy with concurrent requests
+        with transaction.atomic():
+            BookRequest.objects.filter(request_isbn=isbn).update(request_count=F('request_count') + 1)
+            book_request.refresh_from_db()
+        
+        # if request.user.is_authenticated:
+        #     UserRequestBook.objects.create(user=request.user, request=book_request)
+        UserRequestBook.objects.create(user=ttemp_user, request=book_request)
+        
+        return Response({
+            'isbn': isbn,
+            'request_count': book_request.request_count
+        }, status=status.HTTP_200_OK)
