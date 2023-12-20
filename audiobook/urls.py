cb70090fa7534f91c5cdebe26070b95803a4a7f4
Yeseong <ohyeesung@naver.com>
2023-12-20 14:12:18 +0900

    feat: voice front html

--- audiobook/urls.py
+++ audiobook/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
 
-app_name = 'audiobook'
+app_name = 'audiobook'  # audiobook:search
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('template', views.template, name='template'),
-    path('custom', views.custom, name='custom'),
+    path('main', views.main, name='main'),
+    path('genre', views.genre, name='genre'),
+    path('search', views.search, name='search'),
+    path('content', views.content, name='content'),
+    path('content/play', views.content_play, name='content_play'),
+    path('voice/custom', views.voice_custom, name='voice_custom'),
+    path('voice/celebrity', views.voice_celebrity, name='voice_celebrity'),
+    path('voice/custom/upload', views.voice_custom_upload, name='vioce_custom_upload'),
+    path('voice/custom/complete', views.voice_custom_complete, name='vioce_custom_complete'),
+    
 ]
+
--- audiobook/views.py
+++ audiobook/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls.base import reverse
 
 
 def index(request):
@@ -9,5 +10,38 @@ def template(request):
     return render(request, 'audiobook/template.html')
 
 
-def custom(request):
-    return render(request, 'audiobook/custom.html')
+def main(request): 
+    pass
+
+
+def genre(request):
+    pass
+
+
+def search(request):
+    pass
+
+
+def content(request):
+    pass
+
+
+def content_play(request):
+    pass
+
+
+def voice_custom(request):
+    return render(request, 'audiobook/voice_custom.html')
+
+
+def voice_celebrity(request):
+    pass
+
+
+def voice_custom_upload(request):
+        return render(request, 'audiobook/voice_custom_upload.html')
+
+
+
+def voice_custom_complete(request):
+    pass
