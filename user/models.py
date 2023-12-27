304b7f7514adfa07c8dd56ffaa7785ce1bb0c02b
hokyun <wjdghrbs2468@naver.com>
2023-12-27 10:21:19 +0900

    fix: Added custom UserManager and updated User model

--- user/models.py
+++ user/models.py
@@ -1,14 +1,46 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _  # i18n에서 사용하는 gettext 함수의 별칭
+import os
+from dotenv import load_dotenv
+load_dotenv()  
 
 
 # Django에 있는 기존의 User model을 상속받아서 사용함
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, oauth_provider, password=None, **extra_fields):
+        if password is None:
+            password = os.getenv("USER_PASSWORD")
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        # Assuming you have a predefined password in your .env file
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=email, nickname=nickname, 
+                          oauth_provider=oauth_provider, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, oauth_provider, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_admin', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must have is_admin=True.')
+        return self.create_user(email, nickname, oauth_provider, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     oauth_provider = models.CharField(max_length=255)
-    username = models.CharField(max_length=20, unique = False)
+    username = models.CharField(max_length=20)
+    email = models.EmailField(unique = True)
+    nickname = models.CharField(max_length = 20)
     user_created_date = models.DateTimeField(auto_now_add=True)
     user_updated_date = models.DateTimeField(auto_now=True)
     user_book_history = ArrayField(models.IntegerField(), null=True)
@@ -16,8 +48,13 @@ class User(AbstractUser):
         models.IntegerField(), null=True, blank=True)
     user_favorite_voices = ArrayField(
         models.IntegerField(), null=True, blank=True)
+    is_active = models.BooleanField(default = True)
     is_admin = models.BooleanField(default=False)
 
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
     # related_name 추가
     groups = models.ManyToManyField(
         'auth.Group',
@@ -39,7 +76,21 @@ class User(AbstractUser):
         related_name="custom_user_set",
         related_query_name="custom_user",
     )
+    def __str__(self):
+        return self.email
 
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+    
 
 class Subscription(models.Model):
     sub_id = models.AutoField(primary_key=True)
