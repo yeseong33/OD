894b0b1b903d0974a0a8e74fddfca8875256b53d
WooyoungPark <wooyoung9654@gmail.com>
2023-12-29 15:19:28 +0900

    feat: voice image and play function

--- audiobook/views.py
+++ audiobook/views.py
@@ -12,6 +12,7 @@ from .serializers import VoiceSerializer
 from .models import *
 from user.views import decode_jwt
 from config.settings import AWS_S3_CUSTOM_DOMAIN
+from django.templatetags.static import static
 
 load_dotenv()
 
@@ -28,18 +29,31 @@ def index(request):
 
 
 # 메인화면
+
+def convert_sample_voice(rvc_path):
+    sample_voice_path = ''
+    return sample_voice_path
+    
 class MainView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'audiobook/main.html'
 
     def get(self, request):
+        # 이달의 TOP 10 책
         top_books = Book.objects.all().order_by('-book_likes')[:10]
-        top_voices = Voice.objects.all().order_by('-voice_like')[:10]
+        
+        # 최근 이용한 책
         user_history_book = []  # 최신 이용한 책 순서로 보이기 위해서 filter를 사용하지 않고 리스트를 만들어서 사용
         for book_id in request.user.user_book_history:
             book = get_object_or_404(Book, book_id=book_id)
             user_history_book.append(book)
         
+        # 이달의 TOP 10 음성
+        top_voices = Voice.objects.all().order_by('-voice_like')[:10]
+        # AI 기능 구현 이후
+        # for voice in top_voices:
+            # voice.voice_sample_path = convert_sample_voice(voice.voice_path) # 객체 속성(필드) 동적 추가
+        # voice.voice_sample_path = static('voices/voice_sample.mp3')
         return Response({
             'top_books': top_books,
             'user_history_book': user_history_book,
