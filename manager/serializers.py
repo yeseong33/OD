b39aa150db2473f310a4bb8ee005c33b282207ea
Taro <62031849+foreverfl@users.noreply.github.com>
2024-01-02 21:37:06 +0900

    Revert "subscription graph"

--- manager/serializers.py
+++ manager/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from audiobook.models import Book
-from user.models import Subscription
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
--- manager/urls.py
+++ manager/urls.py
@@ -11,8 +11,7 @@ urlpatterns = [
     path('book/register/complete/', views.BookRegisterCompleteView.as_view(), name='book_register_complete'),
     
     path('inquiry/', views.inquiry, name='inquiry'),
-    path('subscription/', views.show_subscription, name='subscription'),
-    path('api/subscription-count/', views.SubscriptionCountAPIView.as_view(), name='api_subscription_count'),  # API 엔드포인트 URL
+    path('revenue/', views.revenue, name='revenue'),
     path('faq/', views.faq, name='faq'),
 
     # 개인정보처리
