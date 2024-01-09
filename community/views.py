3f94f199b9d4fc637c8539be396b7fa369c940d1
Yeseong <ohyeesung@naver.com>
2024-01-09 10:55:58 +0900

    fix: like asynchronous

--- community/views.py
+++ community/views.py
@@ -231,7 +231,7 @@ class BookDetail(APIView):
             book, context=context, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response({'book':serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
