2ba39d3060e4d8a526b979f68db8f67c278bbc28
foreverfl <forever_fl@naver.com>
2024-01-10 10:37:46 +0900

    hotfix: Resolve bug

--- audiobook/views.py
+++ audiobook/views.py
@@ -1,5 +1,6 @@
 import os
 import time
+import shutil
 import socket
 
 import requests
@@ -14,6 +15,8 @@ from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from django.templatetags.static import static
 from django.core.files import File
 from django.core.files.storage import default_storage
@@ -30,7 +33,7 @@ from .serializers import VoiceSerializer
 from .models import *
 from user.views import decode_jwt
 from community.models import BookRequest
-from config.settings import AWS_S3_CUSTOM_DOMAIN, MEDIA_URL, FILE_SAVE_POINT
+from config.settings import AWS_S3_CUSTOM_DOMAIN, MEDIA_URL, MEDIA_ROOT, FILE_SAVE_POINT
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -281,7 +284,6 @@ class ContentPlayHTML(APIView):
             'user': request.user,
             'user_favorites': user_favorite_books
         }
-        print(user_favorite_books)
         return Response(context, template_name=self.template_name)
 
     def post(self, request, book_id):
@@ -290,7 +292,6 @@ class ContentPlayHTML(APIView):
         if 'selectedVoiceId' in request.COOKIES:
             # 선택된 쿠키가 존재하는 경우
             selected_voice_id = request.COOKIES.get('selectedVoiceId')
-            print("came in")
             print(f"selectedVoiceId 쿠키 값: {selected_voice_id}")
         else:
             # 선택된 쿠키가 존재하지 않는 경우
@@ -298,7 +299,7 @@ class ContentPlayHTML(APIView):
 
         # 톤
         tone = data.get('tone', 0)
-        print(tone)
+        print('tone:', tone)
 
         # 도서 객체
         book = Book.objects.get(pk=book_id)
@@ -306,10 +307,11 @@ class ContentPlayHTML(APIView):
             get_file_path(), book.book_content_path.path)
         print('book_content_file_path:', book_content_file_path)
 
+        # txt 파일 내용 출력: 이거로 tts를 활용해서 mp3 파일을 만들면 됩니다.
         try:
             with open(book_content_file_path, 'r', encoding='utf-8') as file:
                 content = file.read()
-                print(content)  # 파일 내용 출력
+                print('content:', content)
         except IOError as e:
             print("파일을 읽는 중 오류가 발생했습니다:", e)
 
@@ -317,59 +319,73 @@ class ContentPlayHTML(APIView):
         voice = Voice.objects.get(
             pk=selected_voice_id)
         voice_name = voice.voice_name
-        voice_file_path = voice.voice_path.path  # RVC 파일의 경로
-        print(voice_file_path)
+        voice_sample_path = voice.voice_sample_path.path
 
-        # mp3 파일 생성 후 book 객체에 저장하기
-        # SSH 클라이언트 생성
-        client = paramiko.SSHClient()
-        # 호스트 키 자동으로 수락
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # SSH 연결 (키 기반 인증)
-        client.connect(hostname=hostname, username=username,
-                       key_filename=key_filename)
+        # 파일 이름을 book_title로 변경
+        new_file_name = f"{book.book_title}.mp3"
+        print('new_file_name:', new_file_name)
 
-        sftp = client.open_sftp()
+        # Book 객체에 파일 저장
+        book.book_voice_path.save(new_file_name, File(
+            open(voice_sample_path, 'rb')), save=True)
 
-        # 파일을 전송할 원격 경로
-        remote_file_path = f'/home/kimyea0454/project-main/assets/weights/{voice_name}.pth'
+        # 파일이 성공적으로 저장되었는지 확인
+        if book.book_voice_path:
+            print(f"파일이 저장되었습니다: {book.book_voice_path.url}")
+        else:
+            print("파일 저장에 실패했습니다.")
+            return JsonResponse({'error': 'Failed to save file'}, status=500)
 
-        # 파일 전송(voice.voice_path에 있는 pth 파일을 sftp로 업로드)
-        sftp.put(voice_file_path, remote_file_path)
+        # # mp3 파일 생성 후 book 객체에 저장하기
+        # # SSH 클라이언트 생성
+        # client = paramiko.SSHClient()
+        # # 호스트 키 자동으로 수락
+        # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # # SSH 연결 (키 기반 인증)
+        # client.connect(hostname=hostname, username=username,
+        #                key_filename=key_filename)
 
-        # 셸 세션 열기
-        shell = client.invoke_shell()
+        # sftp = client.open_sftp()
 
-        commands = [
-            f'python3 tts.py {content}\n',
-            'cd project-main\n',
-            f'python3 inference.py {voice_name} {tone} audios/tts.mp3\n',
-            'rm -rf audios/tts.mp3\n',
-            f'rm -rf assets/weights/{voice_name}.pth\n',
-        ]
+        # # 파일을 전송할 원격 경로
+        # remote_file_path = f'/home/kimyea0454/project-main/assets/weights/{voice_name}.pth'
 
-        for cmd in commands:
-            shell.send(cmd)
-            # 각 명령의 실행이 끝날 때까지 기다림
-            output = receive_until_prompt(shell, prompt='$ ')
-            print(output)  # 받은 출력을 표시함
+        # # 파일 전송(voice.voice_path에 있는 pth 파일을 sftp로 업로드)
+        # sftp.put(voice_file_path, remote_file_path)
 
-        # 임시 저장한 로컬 파일을 원격 시스템으로 업로드
-        remote_path = f'/home/kimyea0454/project-main/audios/{voice_name}.mp3'
-        project_path = os.getcwd()
-        folder_path = os.path.join(project_path, 'static', 'temp')  # 경로 조합
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+        # # 셸 세션 열기
+        # shell = client.invoke_shell()
 
-        # 로컬로 mp3 다운로드(로컬에 받고 db에 저장)
-        sftp.get(remote_path, os.path.join(
-            project_path, f'static/temp/{voice_name}.mp3'))
+        # commands = [
+        #     f'python3 tts.py {content}\n',
+        #     'cd project-main\n',
+        #     f'python3 inference.py {voice_name} {tone} audios/tts.mp3\n',
+        #     'rm -rf audios/tts.mp3\n',
+        #     f'rm -rf assets/weights/{voice_name}.pth\n',
+        # ]
 
-        # SFTP 세션 종료
-        sftp.close()
-        client.close()
+        # for cmd in commands:
+        #     shell.send(cmd)
+        #     # 각 명령의 실행이 끝날 때까지 기다림
+        #     output = receive_until_prompt(shell, prompt='$ ')
+        #     print(output)  # 받은 출력을 표시함
 
-        return JsonResponse({'Good': '변환완료'}, status=200)
+        # # 임시 저장한 로컬 파일을 원격 시스템으로 업로드
+        # remote_path = f'/home/kimyea0454/project-main/audios/{voice_name}.mp3'
+        # project_path = os.getcwd()
+        # folder_path = os.path.join(project_path, 'static', 'temp')  # 경로 조합
+        # if not os.path.exists(folder_path):
+        #     os.makedirs(folder_path)
+
+        # # 로컬로 mp3 다운로드(로컬에 받고 db에 저장)
+        # sftp.get(remote_path, os.path.join(
+        #     project_path, f'static/temp/{voice_name}.mp3'))
+
+        # # SFTP 세션 종료
+        # sftp.close()
+        # client.close()
+
+        return JsonResponse({'success': 'true'}, status=200)
 
 
 # 성우
@@ -464,8 +480,6 @@ class voice_custom_upload(APIView):
             voice_name = data['voice_name']
             voice_photo = request.FILES['voice_photo']
             voice_file = request.FILES['voice_file']  # client에서 받아온 mp3 파일
-            print(voice_photo)
-            print(voice_file)
 
             if Voice.objects.filter(voice_name=voice_name).exists():
                 return JsonResponse({'error': '이미 존재하는 이름입니다.'}, status=400)
@@ -550,6 +564,14 @@ class voice_custom_upload(APIView):
 class voice_custom_complete(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'audiobook/voice_custom_complete.html'
+
+    # 파일 경로 조정 및 저장
+    @staticmethod
+    def save_file(instance, file_field, temp_file):
+        if temp_file:
+            file_name = os.path.basename(temp_file.name)
+            new_path = file_name
+            file_field.save(new_path, temp_file, save=False)
 
     def get(self, request, book_id):
         book = get_object_or_404(Book, pk=book_id)
@@ -640,14 +662,8 @@ class voice_custom_complete(APIView):
                 public_status = data.get('public') == 'true'
                 tone = data.get('tone')
 
-                # 파일 경로 정보 출력
+                # TemporaryFile id 출력
                 print('temp_voice_id:', temp_voice_id)
-                print('temp_voice.temp_voice_image_path:',
-                      temp_voice.temp_voice_image_path)
-                print('temp_voice.temp_voice_sample_path:',
-                      temp_voice.temp_voice_sample_path)
-                print('temp_voice.temp_voice_rvc_path:',
-                      temp_voice.temp_voice_rvc_path)
 
                 # # param 조정된 rvc를 voice_path에 저장
                 # # SSH 클라이언트 생성
@@ -699,7 +715,14 @@ class voice_custom_complete(APIView):
                 # # SFTP 세션 종료
                 # sftp_client.close()
 
-                # Voice 인스턴스 생성 및 저장
+                # project_path = os.getcwd()
+                # with open(os.path.join(project_path, f'static/tts/{voice_name}.mp3'), 'rb') as file:
+                #     voice_sample = ContentFile(file.read())
+                # with open(f'static/tts/{voice_name}.pth', 'rb') as file:
+                #     voice_model = ContentFile(file.read())
+
+                # Voice 인스턴스 생성
+                print("voice 인스턴스 생성")
                 voice_data = {
                     'voice_name': voice_name,  # 사용자 입력
                     'voice_like': 0,
@@ -708,35 +731,23 @@ class voice_custom_complete(APIView):
                     'user': request.user.user_id,
                 }
 
-                # project_path = os.getcwd()
-                # with open(os.path.join(project_path, f'static/tts/{voice_name}.mp3'), 'rb') as file:
-                #     voice_sample = ContentFile(file.read())
-                # with open(f'static/tts/{voice_name}.pth', 'rb') as file:
-                #     voice_model = ContentFile(file.read())
-
-                print("staic/tts 파일 로딩 완료")
-
+                # Voice 인스턴스 저장
+                print("voice 인스턴스 저장")
                 serializer = VoiceSerializer(data=voice_data)
                 if serializer.is_valid():
                     voice_instance = serializer.save()
-                    print("voice 인스턴스 생성 완료")
                     # voice_instance.voice_sample_path.save(
                     #     f"{voice_name}.mp3", voice_sample, save=False)
                     # voice_instance.voice_path.save(
                     #     f"{voice_name}.pth", voice_model, save=False)
 
-                # temp_voice에서 정보를 가져와서 voice instance에 저장
-                if temp_voice.temp_voice_image_path:
-                    voice_instance.voice_image_path.save(
-                        temp_voice.temp_voice_image_path.name, temp_voice.temp_voice_image_path, save=False)
-
-                if temp_voice.temp_voice_sample_path:
-                    voice_instance.voice_sample_path.save(
-                        temp_voice.temp_voice_sample_path.name, temp_voice.temp_voice_sample_path, save=False)
-
-                if temp_voice.temp_voice_rvc_path:
-                    voice_instance.voice_path.save(
-                        temp_voice.temp_voice_rvc_path.name, temp_voice.temp_voice_rvc_path, save=False)
+                    # temp_voice에서 정보를 가져와서 voice instance에 저장
+                    self.save_file(voice_instance, voice_instance.voice_image_path,
+                                   temp_voice.temp_voice_image_path)
+                    self.save_file(voice_instance, voice_instance.voice_sample_path,
+                                   temp_voice.temp_voice_sample_path)
+                    self.save_file(voice_instance, voice_instance.voice_path,
+                                   temp_voice.temp_voice_rvc_path,)
 
                     voice_instance.save()
 
@@ -747,8 +758,6 @@ class voice_custom_complete(APIView):
                         'message': 'Registration failed.',
                         'errors': serializer.errors
                     }, status=501)
-
-                del request.session['voice_creation']
 
                 # # static/tts에 있는 파일 삭제
                 # os.remove(os.path.join(project_path,
@@ -773,53 +782,13 @@ class voice_custom_complete(APIView):
                 # # 연결 종료
                 # client.close()
 
-                # 임시 자료 삭제
+                # 세션 삭제
+                del request.session['voice_creation']
+
+                # TemporaryFile DB에서 제거
                 temp_voice.delete()
 
                 redirect_url = reverse('audiobook:voice_custom', kwargs={
-                                       'book_id': book_id})
-                return JsonResponse({'redirect_url': redirect_url})
-
-        else:
-            voice_creation_data = request.session.get(
-                'voice_creation')  # 세션 데이터
-
-            if voice_creation_data:
-                voice_name = voice_creation_data['voice_name']
-                temp_voice_photo_id = voice_creation_data['temp_voice_photo_id']
-                # SSH 클라이언트 생성
-                client = paramiko.SSHClient()
-                # 호스트 키 자동으로 수락
-                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                # SSH 연결 (키 기반 인증)
-                client.connect(hostname=hostname, username=username,
-                               key_filename=key_filename)
-
-                temp_voice_photo = TemporaryFile.objects.get(
-                    id=temp_voice_photo_id)
-
-                voice_image.delete()
-
-                # 셸 세션 열기
-                shell = client.invoke_shell()
-
-                commands = [
-                    'cd project-main\n',
-                    f'rm -rf assets/weights/{voice_name}.pth\n',
-                    f'rm -rf audios/{voice_name}.mp3\n',
-                    f'rm -rf logs/{voice_name}\n',
-                    'rm -rf voices\n',
-                    'mkdir voices\n',
-                ]
-
-                for cmd in commands:
-                    shell.send(cmd)
-                    # 각 명령의 실행이 끝날 때까지 기다립니다.
-                    output = receive_until_prompt(shell, prompt='$ ')
-                    print(output)  # 받은 출력을 표시합니다.
-
-                client.close()
-                redirect_url = reverse('audiobook:voice_custom_upload', kwargs={
                                        'book_id': book_id})
                 return JsonResponse({'redirect_url': redirect_url})
 
@@ -841,8 +810,6 @@ class VoiceLikeView(APIView):
         voice_id = int(request.GET.get('voice_id'))
         voice = Voice.objects.get(pk=voice_id)
 
-        print('통과1')
-
         if voice_id in map(int, user.user_favorite_voices):
             user.user_favorite_voices.remove(voice_id)
             voice.voice_like -= 1
@@ -855,11 +822,9 @@ class VoiceLikeView(APIView):
             like = True
             print(
                 f"성우 이름 : {voice.voice_name}, voice_id : {voice.voice_id} 좋아요 완료함")
-        print('통과2')
 
         user.save()
         voice.save()
-        print('통과3')
 
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             return JsonResponse({'success': True})
